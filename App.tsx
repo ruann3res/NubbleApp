55b6cfee945e020c1d52a4259b0f6ae495a98ba0
Ruann3res <ruanneres@pecege.com>
2023-09-27 12:08:02 -0300

    feat: finished icon component

--- App.tsx
+++ App.tsx
@@ -1,22 +1,17 @@
 import {ThemeProvider} from '@shopify/restyle';
 import React from 'react';
 import {SafeAreaView, View} from 'react-native';
-import {Button, Text} from './src/components';
+import {Text} from './src/components';
 import {theme} from './src/theme/Theme';
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaView>
-        <View style={{paddingHorizontal: 24, backgroundColor: 'black'}}>
+        <View style={{paddingHorizontal: 24}}>
           <Text preset="headingLarge" italic bold>
             NUBBLE App
           </Text>
-
-          <Button title="Click me" mb="s12" />
-          <Button title="Outline" mb="s12" disabled preset="outline" />
-          <Button title="Outline" mb="s12" disabled preset="primary" />
-          <Button loading title="mensagem" mt="s10" />
         </View>
       </SafeAreaView>
     </ThemeProvider>
--- src/assets/icons/EyeOffIcon.tsx
+++ src/assets/icons/EyeOffIcon.tsx
@@ -1,11 +1,8 @@
+import React from 'react';
 import {Svg, Path} from 'react-native-svg';
+import {IconBase} from '../../components/Icon/Icon';
 
-interface IconProps {
-  color?: string;
-  size?: number;
-}
-
-export function EyeOffIcon({color = 'blue', size}: IconProps) {
+export function EyeOffIcon({size = 20, color = 'black'}: IconBase) {
   return (
     <Svg width={size} height={size} viewBox="0 0 20 20" fill="none">
       <Path
